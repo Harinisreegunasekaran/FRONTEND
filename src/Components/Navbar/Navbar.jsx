cc7dd29c56c323f68b9cff07154c71587f7c6143
Harinisreegunasekaran <727722euec054@skcet.ac.in>
2024-08-13 14:21:16 +0530

    Added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
         <div className='navbar'>
             <div className="nav-logo">
                 <img src={logo} alt="logo"/>
-                <p>HAJA</p>
+                <p>StyleSphere</p>
             </div>
             <ul className="nav-menu">
                 <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : null}</li>
