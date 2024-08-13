e52055caf22c9b1f7ca7239a993ebec67899d312
Harinisreegunasekaran <727722euec054@skcet.ac.in>
2024-08-13 20:44:18 +0530

    Added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer= () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>HAJA</p>
+        <p>StyleSphere</p>
       </div>
       <ul className="footer-links">
         <li >Company</li>
