b6b0b15b3a83a7c5c779616cc5519fb3256c34bc
Harinisreegunasekaran <727722euec054@skcet.ac.in>
2024-08-13 12:11:20 +0530

    Front end added

---
+++ src/Components/Assets/new_collections.jsx
@@ -0,0 +1,61 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Crop Tops",
+    image: p1_img,
+    new_price: "RS.500",
+  },
+  {
+    id: 35,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: "Rs.1200",
+  },
+  {
+    id: 14,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p3_img,
+    new_price: "Rs.800",
+  },
+  {
+    id: 8,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p4_img,
+    new_price: "Rs.600",
+  },
+  {
+    id: 15,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p5_img,
+    new_price: "Rs.1000",
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image: p6_img,
+    new_price: "Rs.1500",
+  },
+  {
+    id: 17,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p7_img,
+    new_price: "Rs.900",
+  },
+  {
+    id: 28,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: p8_img,
+    new_price: "Rs.1100",
+  },
+];
+
+export default new_collections;
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
+// import Footer from '../Components/Footer/Footer'
+import ShopCategory from './ShopCategory'
+// import LoginSignup from './LoginSignup'
+// import Breadcrum from '../Components/Breadcrumps/Breadcrum'
+const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Popular/>
+        <Offers/>
+        <NewCollections/>
+        <NewsLetter/>
+        <ShopCategory/>
+        {/* <Breadcrum /> */}
+        {/* <LoginSignup/> */}
+    </div>
+  )
+}
+
+export default Shop
